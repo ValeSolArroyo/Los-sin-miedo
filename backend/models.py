8d6775385e80ca083609656237d21eaae7586e93
Joaquin <jgomezt@fi.uba.ar>
2024-06-30 18:32:32 -0300

    modelo de la tabla juegos

---
+++ backend/models.py
@@ -0,0 +1,12 @@
+import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Juego(db.Model):
+    __tablename__ = 'juegos'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    precio = db.Column(db.Integer, nullable=False)
+    fecha_creacion = db.Column(db.DateTime, default=datetime.datetime.now())
+    # imagen = db.Column(db.String(100), nullable=False)
