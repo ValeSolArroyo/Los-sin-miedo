d48cc29e2b5462789577239c357ad250d70639f9
ValeSolArroyo <165729487+ValeSolArroyo@users.noreply.github.com>
2024-07-03 12:07:30 -0300

    Agregado la ruta registro

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,12 @@
-from flask import Flask, request, jsonify
-from models import db, Juego
+from flask import Flask, request, jsonify, render_template
+from models import db, Juego, User
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../frontend')
 CORS(app)
 
 port = 5000
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://usuario:contrasenia@localhost:5432/tienda'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/testeo'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
@@ -68,27 +68,30 @@ def crear_juego():
         return jsonify({'message': 'Juego creado exitosamente'})
     except Exception as e:
         return jsonify({'message': 'Error al crear juego', 'error': str(e)}), 500
-    
-@app.route('/juegos/<int:id>', methods=['PUT'])
-def editar_juego(id):
-    try:
-        juego = Juego.query.get(id)
 
-        if not juego:
-            return jsonify({'message': 'Juego no encontrado'}), 404
+@app.route('/registro', methods=['GET','POST'])
+def registro():
 
-        data = request.json
+    if request.method == 'POST':
+        try:
+            usuario = request.form['usuario']
+            email = request.form['email']
+            contraseña = request.form['contraseña']
 
-        juego.nombre = data.get('nombre', juego.nombre)
-        juego.precio = data.get('precio', juego.precio)
-        juego.fecha_creacion = data.get('fecha_creacion', juego.fecha_creacion)
-        juego.imagen = data.get('imagen', juego.imagen)
+            nuevo_user = User(usuario=usuario, email=email, contraseña=contraseña)
+            db.session.add(nuevo_user)
+            db.session.commit()
 
-        db.session.commit()
+            return jsonify({'message': 'Usuario creado correctamente'})
+        
+        except KeyError as e:
+            return jsonify({'message': 'Falta un campo requerido', 'error': str(e)}), 400
 
-        return jsonify({'message': 'Juego editado exitosamente'})
-    except Exception as e:
-        return jsonify({'message': 'Error al editar juego', 'error': str(e)}), 500
+        except Exception as e:
+            return jsonify({'message': 'Error interno del servidor', 'error': str(e)}), 500
+        
+    elif request.method == 'GET':
+        return render_template('registrar_user/index.html')
 
 if __name__ == '__main__':
     print('Iniciando servidor...')
