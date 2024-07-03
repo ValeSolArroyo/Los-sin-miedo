506afe71f1918d3a66d94ccb50f8b3c6c98d3238
ValeSolArroyo <165729487+ValeSolArroyo@users.noreply.github.com>
2024-07-03 12:08:07 -0300

    Agregado la clase User

--- backend/models.py
+++ backend/models.py
@@ -10,3 +10,10 @@ class Juego(db.Model):
     precio = db.Column(db.Integer, nullable=False)
     fecha_creacion = db.Column(db.DateTime, default=datetime.datetime.today())
     imagen = db.Column(db.String(200))
+
+class User(db.Model):
+    __tablename__ = 'usuarios'
+    id = db.Column(db.Integer, primary_key=True)
+    usuario = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    contraseña = db.Column(db.String(50), nullable=False)
