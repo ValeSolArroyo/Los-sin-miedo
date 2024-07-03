53bd86f09a514e33d6c0d174ada6e742e4277cef
ValeSolArroyo <165729487+ValeSolArroyo@users.noreply.github.com>
2024-07-03 17:31:58 -0300

    Anadido estilo a la pag

---
+++ frontend/registrar_user/script.js
@@ -0,0 +1,41 @@
+document.getElementById('usuarioForm').addEventListener('submit', manejarEnvioFormulario);
+
+function manejarEnvioFormulario(event) {
+    event.preventDefault();
+
+    const usuario = document.getElementById('usuario').value;
+    const email = document.getElementById('email').value;
+    const contraseña = document.getElementById('contraseña').value;
+
+    const usuarioData = {
+        usuario: usuario,
+        email: email,
+        contraseña: contraseña
+    };
+
+    enviarUsuario(usuarioData);
+}
+
+function enviarUsuario(usuarioData) {
+    fetch('http://localhost:5000/registro', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(usuarioData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error al registrar usuario.');
+        }
+        return response.json();
+    })
+    .then(data => {
+        alert('Usuario registrado exitosamente!');
+        window.location.href = 'http://localhost:8000/';
+    })
+    .catch(error => {
+        console.error('Error al registrar usuario:', error);
+        alert('Error al registrar usuario.');
+    });
+}
