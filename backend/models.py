d597acf84d86947e529a80f6555b49e9a0d21311
ValeSolArroyo <165729487+ValeSolArroyo@users.noreply.github.com>
2024-07-04 17:54:48 -0300

    Arreglo de las tablas, mejora de relacion entre tablas

--- backend/models.py
+++ backend/models.py
@@ -11,6 +11,10 @@ class Juego(db.Model):
     fecha_creacion = db.Column(db.DateTime, default=datetime.datetime.today())
     imagen = db.Column(db.String(200))
 
+    usuarios = db.relationship('UserJuego', back_populates='juego')
+
+
+
 class User(db.Model):
     __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
@@ -18,11 +22,15 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True, nullable=False)
     contraseña = db.Column(db.String(50), nullable=False)
 
+    juegos = db.relationship('UserJuego', back_populates='usuario')
+
+
 class UserJuego(db.Model):
-    __tablename__='usuario_juegos'
+    __tablename__ = 'usuario_juegos'
     id = db.Column(db.Integer, primary_key=True)
-    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
     id_juego = db.Column(db.Integer, db.ForeignKey('juegos.id'), nullable=False)
-    fecha_creacion = db.Column(db.DateTime, default=datetime.datetime.now) #or today()?
-    usuario = db.relationship('Usuarios')
-    juego = db.relationship('Juegos')
+    fecha_creacion = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    usuario = db.relationship('User', foreign_keys=[id_usuario], back_populates='juegos')
+    juego = db.relationship('Juego', foreign_keys=[id_juego], back_populates='usuarios')
